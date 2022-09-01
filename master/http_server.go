380f1adaed178284f15a4ef0879eb244ee3b624c
leonrayang <chl696@sina.com>
2022-09-01 09:40:39 +0800

    enhance: protect verlist info metanode,decommission support snapshot,enhance 2phase version create
    1)reconstruct and clean code
    2)fix bugs....
    
    Signed-off-by: leonrayang <chl696@sina.com>

--- master/http_server.go
+++ master/http_server.go
@@ -254,9 +254,6 @@ func (m *Server) registerAPIRoutes(router *mux.Router) {
 		Path(proto.AdminDeleteVol).
 		HandlerFunc(m.markDeleteVol)
 	router.NewRoute().Methods(http.MethodGet, http.MethodPost).
-		Path(proto.AdminGetVolVer).
-		HandlerFunc(m.getVolVer)
-	router.NewRoute().Methods(http.MethodGet, http.MethodPost).
 		Path(proto.AdminUpdateVol).
 		HandlerFunc(m.updateVol)
 	router.NewRoute().Methods(http.MethodGet, http.MethodPost).
@@ -297,6 +294,9 @@ func (m *Server) registerAPIRoutes(router *mux.Router) {
 	router.NewRoute().Methods(http.MethodGet).
 		Path(proto.AdminGetAllVersionInfo).
 		HandlerFunc(m.GetAllVersionInfo)
+	router.NewRoute().Methods(http.MethodGet, http.MethodPost).
+		Path(proto.AdminGetVolVer).
+		HandlerFunc(m.getVolVer)
 
 	// S3 lifecycle configuration APIS
 	router.NewRoute().Methods(http.MethodGet, http.MethodPost).
